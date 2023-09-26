19f7d13a6f86f88078eaa58e73a028928a0675ca
Asitha Jayathilake <asithagihan03@gmail.com>
2023-09-26 21:17:16 +1000

    Fix

--- cdk/lib/api-stack.ts
+++ cdk/lib/api-stack.ts
@@ -1,42 +1,51 @@
 import { Construct } from "constructs";
 import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { RestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { AuthResource } from "./auth-resource";
-import { GraphqlApi } from "./graphql-api";
-import { TableResource } from "./table-resource";
-import { OperationTemplateResolver } from "./resolvers/operation-template-resolver";
+import { FunctionResource } from "./function-resource";
+import { Code } from "aws-cdk-lib/aws-lambda";
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const STAGE: string = process.env.STAGE || "stage";
+    const STAGE: string = process.env.STAGE || "dev";
 
     const cognito = new AuthResource(this, "AppUserPoolResource", {
       stage: STAGE,
     });
 
-    const graphqlApi = new GraphqlApi(this, "AppGraphqlApiResource", {
-      stage: STAGE,
-      name: "app-api",
-      schemaPath: "./../graphql/schema.graphql",
-      userPool: cognito.userPool,
+    const api = new RestApi(this, "api", {
+      description: "example api gateway",
+      deployOptions: {
+        stageName: "dev",
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          "Content-Type",
+          "X-Amz-Date",
+          "Authorization",
+          "X-Api-Key",
+        ],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["http://localhost:3000"],
+      },
     });
 
-    const tableResource = new TableResource(this, "DynamoDbTable", {
+    new CfnOutput(this, "apiUrl", { value: api.url });
+
+    const expressApp = new FunctionResource(this, `ApiLambdaFunction`, {
       stage: STAGE,
-      tableEnvValue: "OPERATION_TEMPLATE_TABLE",
-      tableName: "OperationTemplate",
-      partitionKeyName: "id",
+      handler: "lambda.handler",
+      code: Code.fromAsset("./../api/dist/", {}),
     });
 
-    const operationTemplateResource = new OperationTemplateResolver(
-      this,
-      "OperationTemplateResource",
-      {
-        stage: STAGE,
-        tableResources: [tableResource],
-        dataSourceName: "OperationTemplateDs",
-        api: graphqlApi.api,
-      }
+    const anyPath = api.root.addResource("{proxy+}");
+
+    anyPath.addMethod(
+      "ANY",
+      new LambdaIntegration(expressApp.function, { proxy: true })
     );
 
     // Prints out the stack region to the terminal
